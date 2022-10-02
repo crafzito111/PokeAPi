a59c0848c37ac77c60e6f02c6f91b8d295785c5f
crafzito11 <carlosluisprad2@gmail.com>
2022-10-02 17:46:40 -0400

    02-10

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -20,47 +20,54 @@ const PokemonDetails = () => {
   console.log(pokeInfo)
 
   return (
-<div>
-      <HeaderPokedex />
-      <div className='card__detalles_contain'>
-        <div className='card__details'>
-          <div className='card__details_img'>
-            <img className='animate__animated animate__fadeInDown' src={pokeInfo?.sprites.other['official-artwork'].front_default} alt="" />
-          </div>
+     <div>
+        <HeaderPokedex />
+        <div className="card__detalles_contain">
+           <div className={`card__details ${pokeInfo?.types[0].type.name}`}>
+              <div className="card__details_img">
+                 <img
+                    className="animate__animated animate__fadeInDown"
+                    src={pokeInfo?.sprites.other['official-artwork'].front_default}
+                    alt=""
+                 />
+              </div>
 
-          <div className='card__details_title'>
-            <h1 className='card__details_name animate__animated animate__fadeInLeft'>{pokeInfo?.name}</h1>
-          </div>
+              <div className="card__details_title">
+                 <h1 className="card__details_name animate__animated animate__headShake">
+                    {pokeInfo?.name}
+                 </h1>
+              </div>
+           </div>
 
-        </div>
+           <div className="card__details__stacks">
+              {pokeInfo?.stats.map((statss) => (
+                 <div className="stadisticas  animate__animated animate__flash">
+                    <h2>{statss.stat.name}</h2>
+                    <span>{statss['base_stat']}</span>
+                 </div>
+              ))}
+           </div>
 
-        <div className="card__details__stacks">
-          
-{
-  pokeInfo?.stats.map(statss => (
-    <div className="stadisticas  animate__animated animate__flash">
+           <div className="card__abilities  ">
+              {pokeInfo?.moves.map(
+                 (habilidades, index) =>
+                    index < 100 && (
+                       <p
+                          className="habilidades animate__animated animate__pulse"
+                          key={habilidades.order}
+                       >
+                          {habilidades.move.name}
+                       </p>
+                    )
+              )}
+           </div>
 
-      <h2>{statss.stat.name}</h2>
-      <span>{statss['base_stat']}</span>
-    </div>
-  ))
-}
-        </div>
-
-        <div className="card__abilities  ">
-          {
-            pokeInfo?.moves.map((habilidades, index) => index < 100 && (
-              <p className='habilidades animate__animated animate__pulse' key={habilidades.order}>{habilidades.move.name}</p>
-            ))
-          }
-        </div>
-
-   {/* <div className="type">
+           {/* <div className="type">
           <span className='firea'>{pokeInfo?.types[0].type.name}</span>
    </div> */}
-      </div>
-</div>
-  )
+        </div>
+     </div>
+  );
 }
 
 export default PokemonDetails
